7b02e933299b75f1d7c7827e773d087a97958d40
shuboshakuro <shubosha.kuro@gmail.com>
2015-06-01 08:30:24 +0200

    Debug (GUI)

--- src/de/Kana/App.cs
+++ src/de/Kana/App.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Windows.Forms;
+using Kana.src.de.Kana.GUI;
 using Kana.src.de.Kana.Util;
 using Kana.src.de.Kana.Util.XML;
 
@@ -9,12 +10,20 @@ namespace Kana
 	public class App
 	{
 		public static void Main(string[] args) {
+            DebugForm debugWindow = new DebugForm();
+
             WordGraph graph = new WordGraph();
 
             ImportXml.XmlToWordGraph("vocals.xml", out graph);
 
             foreach (Vocable v in graph.getContent())
                 System.Console.WriteLine(v);
+
+            debugWindow.showItems(graph.getContent());
+
+
+            Application.EnableVisualStyles();
+            Application.Run(debugWindow);
         }
 	}
 }
---
+++ src/de/Kana/GUI/GUI_DEBUG.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Kana.src.de.Kana.GUI {
+    public partial class DebugForm : Form {
+        public DebugForm() {
+            InitializeComponent();
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        public void showItems(LinkedList<Vocable> items) {
+            foreach (object obj in items) {
+                debugBox.Items.Add(obj);
+            }
+            debugBox.Refresh();
+        }
+    }
+}
