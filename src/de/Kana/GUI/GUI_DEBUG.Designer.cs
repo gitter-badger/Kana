c030281f2bb7dde72252ae1f63c56f6fb1f821ba
Zaheylu <bonnanaquichu@gmail.com>
2015-08-21 14:10:18 +0200

    romaji: bold statement

--- src/de/Kana/GUI/GUI_DEBUG.Designer.cs
+++ src/de/Kana/GUI/GUI_DEBUG.Designer.cs
@@ -24,32 +24,22 @@
         /// </summary>
         private void InitializeComponent() {
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
+            this.textBox1.Font = new System.Drawing.Font("MS Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(281, 20);
+            this.textBox1.Size = new System.Drawing.Size(736, 71);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(299, 10);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(253, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // DebugForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(564, 44);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(760, 93);
             this.Controls.Add(this.textBox1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "DebugForm";
@@ -62,6 +52,5 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
     }
 }
