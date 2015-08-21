bb187dba4971edf334648ad595eb1097a0973300
Zaheylu <bonnanaquichu@gmail.com>
2015-08-21 13:56:53 +0200

    romaji: almost

--- src/de/Kana/GUI/GUI_DEBUG.cs
+++ src/de/Kana/GUI/GUI_DEBUG.cs
@@ -12,7 +12,6 @@ namespace Kana.src.de.Kana.GUI
 {
     public partial class DebugForm : Form
     {
-        private Trie TTrie { get; set; }
         private int prevL = 0;
         public DebugForm()
         {
@@ -22,41 +21,24 @@ namespace Kana.src.de.Kana.GUI
 
         private void init()
         {
-            TTrie = new Trie();
-            TTrie.Add("a", "あ");
-            TTrie.Add("i", "い");
-            TTrie.Add("o", "お");
-
-            TTrie.Add("sa", "さ");
-            TTrie.Add("si", "し");
-            TTrie.Add("so", "そ");
-
-            TTrie.Add("ssa", "っさ");
-            TTrie.Add("ssi", "っし");
-            TTrie.Add("sso", "っそ");
-
-            TTrie.Add("sha", "しゃ");
-            TTrie.Add("shi", "し");
-            TTrie.Add("sho", "しょ");
-
-            TTrie.Add("ssha", "っしゃ");
-            TTrie.Add("sshi", "っし");
-            TTrie.Add("ssho", "っしょ");
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             int maxL = 4;
             int len;
-            if (((len = textBox1.Text.Length) == prevL + 1))
+            if (((len = textBox1.TextLength) == prevL + 1))
             {
                 int sel = textBox1.SelectionStart;
                 int n1 = Math.Max(sel - maxL, 0);
                 int n2 = Math.Min(sel - n1, maxL);
-                string news = textBox1.Text.Substring(n1, n2);
-                int res = TTrie.Retrieve(news).Count();
-                button1.Text = "start: [" + n1 + "] length : [" + n2 + "] text: [" + news + "] res: [" + res + "]";
+                var sb = new StringBuilder(textBox1.Text);
+                sb.Remove(n1, n2);
+                sb.Insert(n1, Trie.TTrie.Replace(textBox1.Text.Substring(n1, n2)));
+                textBox1.Text = sb.ToString();
+                textBox1.SelectionStart = textBox1.TextLength;
             }
+<<<<<<< 13857d8ea3500a8a93c35d4745474e76cc44874c
             prevL = textBox1.Text.Length;
         }
 
@@ -68,6 +50,9 @@ namespace Kana.src.de.Kana.GUI
 =======
             button1.Text = TTrie.Replace(textBox1.Text);
 >>>>>>> romaji: how to not do it
+=======
+            prevL = textBox1.TextLength;
+>>>>>>> romaji: almost
         }
     }
 }
